f48a762d42f82f39ec61efb29282e8a43a23cf18
FlynnLee <593029372@qq.com>
2018-09-17 17:01:28 +0800

    to select branches

--- app.js
+++ app.js
@@ -17,44 +17,109 @@ app.get('/app/:app_name', (req, res) => {
   const appsConfig = JSON.parse(
     fs.readFileSync(__dirname + '/conf/apps.json', 'utf-8')
   )
-  const data = {
+
+  const app_name = req.params.app_name
+  const app_config = appsConfig[app_name]
+
+  if (!app_config) {
+    res.render('index', {
+      code: 40001,
+      msg: `未找到匹配的应用： [ ${app_name} ]，请检查配置 apps.json`,
+      app_name,
+      branch_key: '',
+      branch: '',
+      webroot: '',
+      branches: ''
+    })
+    return
+  }
+
+  const webroot = app_config.webroot
+  const webroot_branches = app_config.webroot_branches
+  const branch_key = app_config.branch_key
+
+  if (!branch_key) {
+    res.render('index', {
+      code: 40003,
+      msg: '应用未配置branch_key,请检查配置 apps.json',
+      app_name,
+      branch_key,
+      branch: '',
+      webroot: '',
+      branches: ''
+    })
+    return
+  }
+  if (!webroot_branches) {
+    res.render('index', {
+      code: 40005,
+      msg: '应用未配置webroot_branches,请检查配置 apps.json',
+      app_name,
+      branch_key,
+      branch: '',
+      webroot: '',
+      branches: ''
+    })
+    return
+  }
+  if (!webroot) {
+    res.render('index', {
+      code: 40006,
+      msg: '应用未配置webroot,请检查配置 apps.json',
+      app_name,
+      branch_key,
+      branch: '',
+      webroot: '',
+      branches: ''
+    })
+    return
+  }
+
+  const branch = req.cookies[branch_key]
+
+  if (!fs.existsSync(webroot_branches)) {
+    res.render('index', {
+      code: 40004,
+      msg: `分支根目录不存在 ${webroot_branches}`,
+      app_name,
+      branches: '',
+      webroot: '',
+      branch_key,
+      branch
+    })
+    return
+  }
+
+  let webroot_folder
+  if (!branch) {
+    webroot_folder = webroot
+  } else {
+    webroot_folder = `${webroot_branches}/${branch}`
+  }
+
+  if (!fs.existsSync(webroot)) {
+    res.render('index', {
+      code: 40002,
+      msg: `应用目录不存在 ${webroot}，请检查应用目录是否正确`,
+      app_name,
+      branches: '',
+      webroot: webroot_folder,
+      branch_key,
+      branch
+    })
+    return
+  }
+
+  const branches = fs.readdirSync(webroot_branches).join(',')
+  res.render('index', {
     code: 200,
-    app_name: '',
-    branch_key: '',
-    webroot: '',
-    branch: '',
-    msg: ''
-  }
-  data.app_name = req.params.app_name
-  const appConfig = appsConfig[data.app_name]
-  if (!appConfig) {
-    data.code = 40001
-    data.msg = `未找到匹配的应用： [ ${
-      data.app_name
-    } ]，请检查配置 conf/apps.json`
-    console.log(data)
-    res.render('index', data)
-    return
-  }
-
-  data.branch_key = appConfig.branch_key
-  data.webroot = appConfig.webroot
-  data.branch = req.cookies[data.branch_key]
-  if (data.branch) {
-    data.webroot = `${data.webroot}_branches/${data.branch}`
-  }
-
-  if (!fs.existsSync(data.webroot)) {
-    data.code = 40002
-    data.msg = `应用目录不存在 ${data.webroot}，请检查应用是否已构建`
-    console.log(data)
-    res.render('index', data)
-    return
-  }
-
-  data.msg = `应用目录有效 ${data.webroot}`
-  console.log(data)
-  res.render('index', data)
+    msg: `应用目录有效 ${webroot}`,
+    webroot: webroot_folder,
+    app_name,
+    branches,
+    branch,
+    branch_key
+  })
 })
 
 app.listen(PORT, () => {
