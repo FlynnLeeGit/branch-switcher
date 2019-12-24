3c2eb68699142d8c6eb8caa649b5c0c583ed0aa9
FlynnLee <593029372@qq.com>
2019-12-24 15:31:42 +0800

    feat: supoort top level domain,simplify apps.conf

--- app.js
+++ app.js
@@ -1,100 +1,89 @@
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const app = express()
-const path = require('path')
-const logger = require('morgan')
-const fse = require('fs-extra')
-const loadConf = require('./middlewares/load-conf')
-const errors = require('./constants/errors')
-const PORT = 3000
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+const logger = require("morgan");
+const fse = require("fs-extra");
+const loadConf = require("./middlewares/load-conf");
+const errors = require("./constants/errors");
+const PORT = 3000;
 
 app
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .use(express.static('static'))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .use(express.static("static"))
   .use(cookieParser())
   .use(bodyParser.json())
-  .use(logger('dev'))
+  .use(logger("dev"));
 
-app.get('/app/:app_name', loadConf, (req, res) => {
-  const app_name = req.app.get('app_name')
-  const app_config = req.app.get('app_config')
+app.get("/app/:app_name", loadConf, (req, res) => {
+  const app_name = req.app.get("app_name");
+  const app_config = req.app.get("app_config");
 
-  const webroot = app_config.webroot
-  const webroot_branches = app_config.webroot_branches
-  const branch_key = app_config.branch_key
+  const default_branch = app_config.default_branch;
+  const webroot_branches = app_config.webroot_branches;
+  const hostname = req.hostname
 
-  if (!branch_key) {
-    res.render('index', {
-      code: errors.BRANCH_KEY_NO_CONFIGURED,
-      msg: '应用未配置branch_key,请检查配置 apps.json',
-      app_name,
-      branch_key,
-      branch: '',
-      webroot: '',
-      branches: ''
-    })
-    return
-  }
+  console.log(hostname)
+
+  const branch_key = hostname + ".branch";
+
   if (!webroot_branches) {
-    res.render('index', {
+    res.render("index", {
       code: errors.BRANCH_KEY_NO_CONFIGURED,
-      msg: '应用未配置webroot_branches,请检查配置 apps.json',
+      msg: "应用未配置webroot_branches,请检查配置 apps.json",
       app_name,
       branch_key,
-      branch: '',
-      webroot: '',
-      branches: ''
-    })
-    return
-  }
-  if (!webroot) {
-    res.render('index', {
-      code: errors.WEBROOT_NO_CONFIGURED,
-      msg: '应用未配置webroot,请检查配置 apps.json',
-      app_name,
-      branch_key,
-      branch: '',
-      webroot: '',
-      branches: ''
-    })
-    return
+      default_branch: "",
+      branch: "",
+      branches: ""
+    });
+    return;
   }
 
-  const branch = req.cookies[branch_key]
+  if (!default_branch) {
+    res.render("index", {
+      code: errors.BRANCH_KEY_NO_CONFIGURED,
+      msg: "应用未配置default_branch,请检查配置 apps.json",
+      app_name,
+      branch_key,
+      default_branch: "",
+      branch: "",
+      branches: ""
+    });
+    return;
+  }
+
+  const branch = req.cookies[branch_key] || default_branch;
 
   if (!fse.existsSync(webroot_branches)) {
-    res.render('index', {
+    res.render("index", {
       code: errors.BRANCHES_DIR_NOT_FOUND,
       msg: `分支根目录不存在 ${webroot_branches}`,
       app_name,
-      branches: '',
-      webroot: '',
+      default_branch,
+      branches: "",
       branch_key,
       branch
-    })
-    return
+    });
+    return;
   }
 
-  let webroot_folder
-  if (!branch) {
-    webroot_folder = webroot
-  } else {
-    webroot_folder = `${webroot_branches}/${branch}`
-  }
+  const webroot_folder = `${webroot_branches}/${branch}`;
 
-  if (!fse.existsSync(webroot)) {
-    res.render('index', {
+  if (!fse.existsSync(webroot_folder)) {
+    res.render("index", {
       code: errors.WEBROOT_DIR_NOT_FOUND,
-      msg: `应用目录不存在 ${webroot}，请检查应用目录是否正确`,
+      msg: `应用目录不存在 ${webroot_folder}，请检查应用目录是否正确`,
       app_name,
-      branches: '',
+      branches: "",
+      default_branch,
       webroot: webroot_folder,
       branch_key,
       branch
-    })
-    return
+    });
+    return;
   }
 
   const branches = fse
@@ -103,109 +92,110 @@ app.get('/app/:app_name', loadConf, (req, res) => {
       return {
         name,
         path: path.join(webroot_branches, name)
-      }
+      };
     })
     .filter(b => {
-      return fse.existsSync(b.path)
+      return fse.existsSync(b.path);
     })
     .map(b => {
-      b.stats = fse.statSync(b.path)
-      return b
+      b.stats = fse.statSync(b.path);
+      return b;
     })
-    .sort((b1, b2) => b2.stats.mtimeMs - b1.stats.mtimeMs)
+    .sort((b1, b2) => b2.stats.mtimeMs - b1.stats.mtimeMs);
 
-  res.render('index', {
+  res.render("index", {
     code: 200,
     msg: `应用目录有效 ${webroot_folder}`,
     webroot: webroot_folder,
+    default_branch,
     app_name,
     branches,
     branch,
     branch_key
-  })
-})
+  });
+});
 
-app.post('/app/:app_name/gitlab', loadConf, (req, res) => {
-  const app_name = req.app.get('app_name')
-  const app_config = req.app.get('app_config')
+app.post("/app/:app_name/gitlab", loadConf, (req, res) => {
+  const app_name = req.app.get("app_name");
+  const app_config = req.app.get("app_config");
   if (!req.body.repository) {
     res.status(400).json({
       code: errors.NOT_GITLAB_EVENT,
       msg: `need be a gitlab push event`
-    })
-    return
+    });
+    return;
   }
 
-  const event = req.body
-  if (!event.after.includes('00000000000000000')) {
+  const event = req.body;
+  if (!event.after.includes("00000000000000000")) {
     res.status(200).json({
       code: 200,
       data: {},
-      msg: '非删除分支事件'
-    })
-    return
+      msg: "非删除分支事件"
+    });
+    return;
   }
 
-  const webroot_branches = app_config.webroot_branches
-  const branch = event.ref.replace('refs/heads/', '')
-  const branchDir = path.join(webroot_branches, branch)
+  const webroot_branches = app_config.webroot_branches;
+  const branch = event.ref.replace("refs/heads/", "");
+  const branchDir = path.join(webroot_branches, branch);
 
   if (!webroot_branches) {
     res.status(400).json({
       code: errors.BRANCHES_DIR_NOT_FOUND,
       msg: `${app_name}应用未配置webroot_branches,请检查配置 apps.json`
-    })
-    return
+    });
+    return;
   }
 
   try {
-    fse.emptyDirSync(branchDir)
-    fse.removeSync(branchDir)
+    fse.emptyDirSync(branchDir);
+    fse.removeSync(branchDir);
     res.status(200).json({
       code: 200,
       data: {},
-      msg: 'ok'
-    })
+      msg: "ok"
+    });
   } catch (e) {
     res.status(500).json({
       code: 500,
       data: e.stack,
       msg: e.message
-    })
+    });
   }
-})
+});
 
-app.delete('/app/:app_name/delete', loadConf, (req, res) => {
-  const branch = req.body.branch
-  const app_config = req.app.get('app_config')
+app.delete("/app/:app_name/delete", loadConf, (req, res) => {
+  const branch = req.body.branch;
+  const app_config = req.app.get("app_config");
 
-  const webroot_branches = app_config.webroot_branches
-  const branchDir = path.join(webroot_branches, branch)
+  const webroot_branches = app_config.webroot_branches;
+  const branchDir = path.join(webroot_branches, branch);
 
   if (!webroot_branches) {
     res.status(400).json({
       code: errors.BRANCHES_DIR_NOT_FOUND,
       msg: `${app_name}应用未配置webroot_branches,请检查配置 apps.json`
-    })
-    return
+    });
+    return;
   }
   try {
-    fse.emptyDirSync(branchDir)
-    fse.removeSync(branchDir)
+    fse.emptyDirSync(branchDir);
+    fse.removeSync(branchDir);
     res.status(200).json({
       code: 200,
       data: {},
-      msg: 'ok'
-    })
+      msg: "ok"
+    });
   } catch (e) {
     res.status(500).json({
       code: 500,
       data: e.stack,
       msg: e.message
-    })
+    });
   }
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`server started on http://localhost:${PORT}`)
-})
+  console.log(`server started on http://localhost:${PORT}`);
+});
--- readme.md
+++ readme.md
@@ -1,14 +1,16 @@
 # branch-switcher Docker 镜像
 
-
 node 下开发
+现阶段需要配合nginx进行调试
+nginx 配置参考 nginx.conf
 
 ```shell
 npm run dev
 ```
 
 ### 原理
-通过查找配置文件中的 webroot 和 webroot_branches 目录下的文件夹来遍历出分支文件夹名称 供ui下拉选择使用
+
+通过查找配置文件中的 webroot 和 webroot_branches 目录下的文件夹来遍历出分支文件夹名称 供 ui 下拉选择使用
 
 分支切换工具 docker 镜像
 
@@ -20,9 +22,9 @@ apps.json 为配置文件
 
 ```json
 {
-  "demo": { // 项目名称 通过 http://localhost:3030/app/demo 访问
-    "branch_key": "demo_branch", // Cookie的名称
-    "webroot": "/data/htdocs/oa_frontend", // 默认nginx网站根目录
+  "demo": {
+    // 项目名称 通过 http://localhost:3030/app/demo 访
+    "default_branch": "test", // 默认使用分支
     "webroot_branches": "/data/htdocs/oa_frontend_branches" // 多分支用的根目录
   }
 }
@@ -30,5 +32,5 @@ apps.json 为配置文件
 
 ### 使用
 
-* nginx配置 参考./nginx.conf
-* 和gitlab分支删除对应 `/app/:app_name/gitlab`
+- nginx 配置 参考./nginx.conf
+- 和 gitlab 分支删除对应 `/app/:app_name/gitlab`
